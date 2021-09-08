fc6434f60e077626edd4285d0418e8592e08f0f5
Diego <diegocamyy@gmailcom>
2021-09-08 10:17:07 -0300

    Finished hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,52 +1,53 @@
-import {
-  Box,
-  Flex,
-  Text,
-  Heading,
-  useMediaQuery,
-  Tooltip,
-  Icon,
-} from "@chakra-ui/react";
+import { Box, Flex, Text, Heading, useMediaQuery } from "@chakra-ui/react";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
-import profilepic from "../public/me.jpg";
+import { useEffect, useState } from "react";
+import me from "../public/me.png";
 
 function Hero() {
   const [isMobile] = useMediaQuery("(max-width: 786px)");
   const [number, setNumber] = useState(undefined);
 
   useEffect(() => {
-    setNumber(Math.ceil(Math.random() * 100000));
+    setNumber(Math.ceil(Math.random() * 10000));
   }, []);
   return (
-    <Flex as="section" mx="auto" my="20" justify="center" align="center">
+    <Flex
+      as="section"
+      mx="auto"
+      my="20"
+      direction={isMobile ? "column" : "row"}
+      justify="center"
+      align="center"
+    >
+      <Box mx="2">
+        <Image
+          src={me}
+          width="270px"
+          height="350px"
+          objectFit="cover"
+          quality="100"
+        />
+      </Box>
       <Box maxW="550px" mx="2">
         <Heading fontSize="5xl">Hi there!</Heading>
         <Text my="4">
           My name is Diego Camy. Welcome to my website. I'm a self-taught web
           developer from Rivera, Uruguay. To see my work, which has been viewed
           over{" "}
-          <Tooltip label="By me" fontSize="md" hasArrow aria-label="A label">
-            <Text as="span" fontWeight="bold" textDecor="underline">
-              {number}
-            </Text>
-          </Tooltip>{" "}
-          times,{" "}
+          <Text as="span" fontWeight="bold" textDecor="underline">
+            {number}
+          </Text>{" "}
+          times{" "}
+          <Text fontSize="xx-small" as="span">
+            (by me)
+          </Text>
+          {", "}
           <Text as="span" color="linkedin.600">
             <Link href="#projects">click here!</Link>{" "}
           </Text>
           Or you can scroll, which is slower and boring.{" "}
         </Text>
-      </Box>
-      <Box>
-        <Image
-          src={profilepic}
-          width="270px"
-          height="350px"
-          objectFit="cover"
-          quality="100"
-        />
       </Box>
     </Flex>
   );
