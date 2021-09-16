668c8bca33a1d77abd8d7e0b406351b649dbb377
Diego <diegocamyy@gmailcom>
2021-09-16 10:21:09 -0300

    More projects added

--- data/data.ts
+++ data/data.ts
@@ -93,6 +93,81 @@ const data = [
     demoURL: "https://sa-radios.herokuapp.com",
     codeURL: "https://github.com/diegocamy/sa-radios",
   },
+  {
+    title: "MCN Chat",
+    intro: "Realtime chat app built with React and socket.io",
+    images: [
+      "https://res.cloudinary.com/diegocamy/image/upload/v1631551627/portfolio/mcn-chat/MCN-Chat_ibemuz.png",
+      "https://res.cloudinary.com/diegocamy/image/upload/v1631551627/portfolio/mcn-chat/MCN-Chat2_y8i2sp.png",
+      "https://res.cloudinary.com/diegocamy/image/upload/v1631551627/portfolio/mcn-chat/MCN-Chat3_ik80h1.png",
+      "https://res.cloudinary.com/diegocamy/image/upload/v1631551628/portfolio/mcn-chat/MCN-Chat4_ptibar.png",
+      "https://res.cloudinary.com/diegocamy/image/upload/v1631551627/portfolio/mcn-chat/MCN-Chat5_lv6uvn.png",
+      "https://res.cloudinary.com/diegocamy/image/upload/v1631551627/portfolio/mcn-chat/MCN-Chat6_t6sqnk.png",
+      "https://res.cloudinary.com/diegocamy/image/upload/v1631551627/portfolio/mcn-chat/MCN-Chat7_xve5cj.png",
+    ],
+    details:
+      "Another mandatory project that I think everyone has. Overall really interesting experience. nline I made this a long time ago but learned a lot about web sockets. nline 5 chat rooms to switch between",
+    stack: ["React", "Node.js", "Express", "Socket.io"],
+    features: [
+      "Real time messaging",
+      "5 chat rooms",
+      "Notifications in chat when a user joins or leave your room",
+    ],
+    demoURL: "https://mcn-chat.herokuapp.com",
+    codeURL: "https://github.com/diegocamy/mcn-chat",
+  },
+  {
+    title: "Escribe YA",
+    intro:
+      "A game about writing the words that appear on the screen as fast as you can",
+    images: [
+      "https://res.cloudinary.com/diegocamy/image/upload/v1631551626/portfolio/escribeYA/EscribeYA_vd4zai.png",
+      "https://res.cloudinary.com/diegocamy/image/upload/v1631551626/portfolio/escribeYA/EscribeYA4_ac75kj.png",
+      "https://res.cloudinary.com/diegocamy/image/upload/v1631551626/portfolio/escribeYA/EscribeYA5_njqnaz.png",
+      "https://res.cloudinary.com/diegocamy/image/upload/v1631551626/portfolio/escribeYA/EscribeYA3_c1ngyo.png",
+      "https://res.cloudinary.com/diegocamy/image/upload/v1631551626/portfolio/escribeYA/EscribeYA2_avcduj.png",
+    ],
+    details:
+      "This was the second game I made with React, and it's another project made a long time ago, I don't have too much to say about it.",
+    stack: ["React"],
+    features: ["Really fun?", "Responsive design I hope"],
+    demoURL: "https://diegocamy.github.io/escribeya",
+    codeURL: "https://github.com/diegocamy/escribeYA/tree/master",
+  },
+  {
+    title: "Poke-Draw",
+    intro: "Draw a pokemon in 30 seconds and be proud of your masterpiece!",
+    images: [
+      "https://res.cloudinary.com/diegocamy/image/upload/v1631551628/portfolio/pokedraw/React-App_vfonhd.png",
+      "https://res.cloudinary.com/diegocamy/image/upload/v1631551628/portfolio/pokedraw/React-App2_yzazbz.png",
+    ],
+    details:
+      "So... you like pokemon? How about drawing them in 30 seconds? nline I know what you think: 'Why would someone play this?' and the answer is: I DON'T KNOW but I like it. nline Give it a try and have a laugh with your drawing",
+    stack: ["React"],
+    features: ["Really fun?????", "Gotta draw 'em all"],
+    demoURL: "https://diegocamy.github.io/pokedraw",
+    codeURL: "https://github.com/diegocamy/pokedraw/tree/master/src",
+  },
+  {
+    title: "Memoria",
+    intro: "Memory matching game",
+    images: [
+      "https://res.cloudinary.com/diegocamy/image/upload/v1631551628/portfolio/memoria/React-App_bv84p6.png",
+      "https://res.cloudinary.com/diegocamy/image/upload/v1631551628/portfolio/memoria/React-App2_qqfqqv.png",
+      "https://res.cloudinary.com/diegocamy/image/upload/v1631551627/portfolio/memoria/React-App3_hjso3w.png",
+      "https://res.cloudinary.com/diegocamy/image/upload/v1631551628/portfolio/memoria/React-App4_bore6g.png",
+    ],
+    details:
+      "Another AAA game made by me, but I didn't spend 2 days making this one like I did with the other two and it's probably good for your brain. nline This one I actually played a couple of times when I was bored so it's useful",
+    stack: ["React", "Typescript"],
+    features: [
+      "Really fun, sometimes",
+      "Actually good for your brain",
+      "Responsive design",
+    ],
+    demoURL: "https://diegocamy.github.io/memoria",
+    codeURL: "https://github.com/diegocamy/memoria",
+  },
 ];
 
 export default data;
