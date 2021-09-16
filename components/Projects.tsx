a898761db38b1b6caeabb48f43d83c6397b93e4a
Diego <diegocamyy@gmailcom>
2021-09-16 10:51:13 -0300

    Added a github link to Projects component

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,5 +1,15 @@
-import { Flex, Heading, useColorMode } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  Heading,
+  IconButton,
+  Link,
+  useColorMode,
+} from "@chakra-ui/react";
 import React from "react";
+import { FaGit, FaGithub } from "react-icons/fa";
+import useButtonColor from "../hooks/useButtonColor";
+import useHovering from "../hooks/useHovering";
 import { ProjectObject } from "../pages";
 import Project from "./Project";
 
@@ -8,7 +18,9 @@ interface Props {
 }
 
 function Projects({ projects }: Props) {
-  const { colorMode, toggleColorMode } = useColorMode();
+  const { colorMode } = useColorMode();
+  const { hovering, setHovering } = useHovering();
+  const { buttonColor, buttonTextColor } = useButtonColor(colorMode, hovering);
 
   return (
     <Flex
@@ -25,6 +37,27 @@ function Projects({ projects }: Props) {
       {projects.map((p, idx) => (
         <Project key={idx} inverted={idx % 2 === 1} project={p} />
       ))}
+      <Heading as="h3" size="md" mt="10" mb="5">
+        Check out my GitHub to see more projects
+      </Heading>
+      <Button
+        leftIcon={<FaGithub />}
+        mb="10"
+        fontSize="xl"
+        as="a"
+        href="https://github.com/diegocamy"
+        target="_blank"
+        bg={buttonColor}
+        color={buttonTextColor}
+        _hover={{ bg: buttonColor, color: buttonTextColor }}
+        onMouseEnter={() => setHovering(true)}
+        onMouseLeave={() => setHovering(false)}
+        onClick={() => {
+          setHovering(false);
+        }}
+      >
+        diegocamy
+      </Button>
     </Flex>
   );
 }
